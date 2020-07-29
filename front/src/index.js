f2000bd080a409743e2481c1ecee3ff4ae0939bf
eric martinez <eamartinez.acelas@outlook.com>
2020-07-28 23:32:50 -0500

    render variables con listas

--- front/src/index.js
+++ front/src/index.js
@@ -7,10 +7,15 @@ contenedor.appendChild(element);
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const name = 'Eric'
+const name = {
+  frist: 'Eric',
+  second: 'Martinez',
+  age: 28
+}
 //const element = <h1>Hello with ReactJS and React Dom</h1>
 function element2(name){
-  return <span>Created By: {name}</span>
+return <span>Created By: {name.frist} {name.second}, Edad: {name.age}</span>
+
 }
 
 const container = document.getElementById('root')
