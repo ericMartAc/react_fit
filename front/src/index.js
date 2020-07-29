211a8fdcf5943e85546b9afca3bad5be913707b4
eric martinez <eamartinez.acelas@outlook.com>
2020-07-28 23:28:26 -0500

    render variables

--- front/src/index.js
+++ front/src/index.js
@@ -7,7 +7,12 @@ contenedor.appendChild(element);
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const element = <h1>Hello with ReactJS and React Dom</h1>
+const name = 'Eric'
+//const element = <h1>Hello with ReactJS and React Dom</h1>
+function element2(name){
+  return <span>Created By: {name}</span>
+}
+
 const container = document.getElementById('root')
 
-ReactDom.render(element,container)
+ReactDom.render(element2(name),container)
