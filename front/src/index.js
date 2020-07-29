950d542aea5f4305f0065c83269bedc118663d39
eric martinez <eamartinez.acelas@outlook.com>
2020-07-29 00:30:07 -0500

    render variables con arreglo y operación y condicionales

--- front/src/index.js
+++ front/src/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 const user = ['Eric', 'Martinez', 1992, 'Barranquilla']
+//const user = null;
 
 function getGetting(user) {
-  const fecha = new Date();
-  var año = fecha.getUTCFullYear();
-return <span>{user[0]} {user[1]} tiene {año - user[2]} años, nacido en {user[3]} </span>
+  if (user) {
+    const fecha = new Date();
+    var año = fecha.getUTCFullYear();
+    return <span>{user[0]} {user[1]} tiene {año - user[2]} años, nacido en {user[3]} </span>
+  }
+  return <span>No existen datos</span>
 }
 
 const container = document.getElementById('root')
