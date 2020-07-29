b7bacda917e0eaf542b312aba1f5e9a8f8c7ad46
eric martinez <eamartinez.acelas@outlook.com>
2020-07-28 22:31:38 -0500

    render con reactDom

--- front/src/index.js
+++ front/src/index.js
@@ -1,4 +1,13 @@
+/*
 const element = document.createElement('h1');
 element.innerHTML = 'Hello with JS';
 const contenedor = document.getElementById('root');
 contenedor.appendChild(element);
+*/
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const element = <h1>Hello with ReactJS and React Dom</h1>
+const container = document.getElementById('root')
+
+ReactDom.render(element,container)
